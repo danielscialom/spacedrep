1fa0b4d1b912a1fc9584ec161f2fbe1bdf3507dc
Daniel <danielscialom1970@gmail.com>
2026-08-23 12:05:18 +0300

    Initial commit

---
+++ src/main/java/spacedrep/SpacedrepApplication.java
@@ -0,0 +1,13 @@
+package spacedrep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpacedrepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpacedrepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spacedrep/SpacedrepApplicationTests.java
@@ -0,0 +1,13 @@
+package spacedrep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpacedrepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
